266e753174e0f6a1553720c2dfca5cda52cf3cbe
michaelbelong <michaelbelong@gmail.com>
2021-01-24 14:00:53 -0800

    Nuked ContentView to working state in order to develop CoreData functionality

--- Counter/ContentView.swift
+++ Counter/ContentView.swift
@@ -9,47 +9,12 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var ViewContext
-    
-    @FetchRequest(sortDescriptors: [])
-    var counters: FetchedResults<Count>
     
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(counters) { count in
-                    CountRow(count: count)
-                }
-            }
-            .navigationTitle("Counter List 1")
-            .navigationBarItems(trailing: Button("Add") {
-                addCount()
-            })
-        }
-    }
-    
-    private func saveContext() {
-        do {
-            try ViewContext.save()
-        } catch {
-            let error = error as NSError
-            fatalError("Unresolved Error: \(error)")
-        }
-    }
-    
-    private func addCount() {
-        withAnimation {
-            let newCount = Count(context: ViewContext)
-            newCount.name = "Counter \(Date())"
-            newCount.color = "red"
-            newCount.timestamp = Date()
-            newCount.value = 0
-            newCount.group = "Current"
-            
-            saveContext()
-        }
+        Text("Hello, Cale.")
     }
 }
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
